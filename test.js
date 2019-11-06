eedfdac55646d813f0f694efd1e52febf27e9159
Hale Deng <haledeng88@gmail.com>
2019-11-06 11:50:39 +0800

    fix: remove useless file

--- test.js
+++
@@ -1,9 +0,0 @@
-const str = '?a=b&c=d'
-
-const u = new URLSearchParams(str)
-
-console.log(u)
-console.log(u.toString())
-
-// console.log(u instanceof URLSearchParams)
-// console.log(undefined instanceof URLSearchParams)
