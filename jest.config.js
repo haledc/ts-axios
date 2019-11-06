9cb1d7dc5211de36feac3c9007f5158e527dc056
Hale Deng <haledeng88@gmail.com>
2019-11-06 11:50:09 +0800

    refactor: extract and update jest and prettier

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  rootDir: __dirname,
+  transform: {
+    '.(ts|tsx)': 'ts-jest'
+  },
+  testEnvironment: 'jsdom',
+  testRegex: '/test/.*\\.(test|spec)\\.(ts)$',
+  moduleFileExtensions: ['ts', 'tsx', 'js'],
+  coveragePathIgnorePatterns: ['/node_modules/', '/test/'],
+  coverageThreshold: {
+    global: {
+      branches: 90,
+      functions: 95,
+      lines: 95,
+      statements: 95
+    }
+  },
+  collectCoverageFrom: ['src/*.{js,ts}', 'src/**/*.{js,ts}'],
+  setupFilesAfterEnv: ['<rootDir>/test/boot.ts']
+}
