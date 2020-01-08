3da839f3bcb1bef781e3209364ff1490dd5e7a49
Hale Deng <haledeng88@gmail.com>
2020-01-08 21:51:43 +0800

    refactor: update constructor location

--- src/cancel/CancelToken.ts
+++ src/cancel/CancelToken.ts
@@ -14,6 +14,18 @@ export default class CancelToken implements CancelTokenInterface {
   promise: Promise<Cancel>
   reason?: Cancel
 
+  constructor(executor: CancelExecutor) {
+    let resolvePromise: ResolvePromise
+    this.promise = new Promise<Cancel>(resolve => (resolvePromise = resolve))
+
+    executor(message => {
+      if (this.reason) return
+
+      this.reason = new Cancel(message)
+      resolvePromise(this.reason)
+    })
+  }
+
   static source(): CancelTokenSource {
     let cancel!: Canceler
     const token = new CancelToken(c => (cancel = c))
@@ -27,17 +39,5 @@ export default class CancelToken implements CancelTokenInterface {
     if (this.reason) {
       throw this.reason
     }
-  }
-
-  constructor(executor: CancelExecutor) {
-    let resolvePromise: ResolvePromise
-    this.promise = new Promise<Cancel>(resolve => (resolvePromise = resolve))
-
-    executor(message => {
-      if (this.reason) return
-
-      this.reason = new Cancel(message)
-      resolvePromise(this.reason)
-    })
   }
 }
